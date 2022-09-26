d3bdb5a58752dc702a580fdb591cc99885e43683
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-09-25 22:43:10 -0500

    Delete Code directory

--- Code/class_objects.py
+++
@@ -1,9 +0,0 @@
-class User:
-    def __init__(self, username, password, first_name=None, middle_name=None, last_name=None, email=None, password_confirmation=None):
-        self.username = username
-        self.password = password
-        self.first_name = first_name
-        self.middle_name = middle_name
-        self.last_name = last_name
-        self.email = email
-        self.password_confirmation = password_confirmation
