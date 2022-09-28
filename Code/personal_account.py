3a271454bbd8f22642531bb8ab08466785bbf3aa
Brett <105253172+BrettBaltz@users.noreply.github.com>
2022-09-28 13:40:52 -0500

    Add files via upload

--- Code/personal_account.py
+++ Code/personal_account.py
@@ -3,21 +3,21 @@ from class_objects import User
 
 app = Flask(__name__)
 
-# TODO: Delete after implementing database
 # User for testing purposes
 test_user = User('test_username', 'test_password', 'test_first', 'test_middle', 'test_name', 'test_email', 'test_password')
 
-# TODO: Delete
-@app.route('/')
-def default():
-    current_user = test_user
-    return render_template('personal_timeline.html', user = current_user)
-
 # User's personal timeline
+@app.route('/') # TODO: Delete
+@app.route('/home')
 @app.route('/home/timeline')
 def personal_timeline():
     current_user = test_user # TODO: Delete
     return render_template('personal_timeline.html', user = current_user)
+
+# @app.route('/home/timeline/new_post')
+# def new_post():
+#     current_user = test_user # TODO: Delete
+#     return render_template('personal_timeline.html', user = current_user, new_post_flag = True)
 
 # User's friends page
 @app.route('/home/friends')
