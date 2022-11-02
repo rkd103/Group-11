d1222105f61dc789c0536a05f1a099ab0ae02497
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-02 08:11:06 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from atexit import register
 import pytest
 import flask_login
 # Imports the instance of the web application instantiated in the file "app.py"
-from Code import project
+from Code import project.db
 from Code import app
 from Code.app import app, RegisterForm
 
