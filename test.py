f0a27117b3cdc007498716e1b535084cb6caba68
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-02 06:02:04 -0500

    Update test.py

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from atexit import register
 import pytest
 import flask_login
 # Imports the instance of the web application instantiated in the file "app.py"
-from Code import app
+import app
 
 #*************************
 #***Fixture Definitions***
