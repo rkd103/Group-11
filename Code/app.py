1834a65041a88275f36951e270c9885dbd3b3fd0
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-02 08:54:34 -0500

    Update app.py

--- Code/app.py
+++ Code/app.py
@@ -2,7 +2,6 @@
 #***Library*Includes***
 #**********************
 from tempfile import tempdir
-from requests import post
 from flask import Flask, render_template, redirect, request, url_for, flash, session, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc
