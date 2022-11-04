35a98aa2f523bd498f77f6964bb3c6542a4e53ee
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-04 15:31:20 -0500

    Update test.py

--- test.py
+++ test.py
@@ -464,6 +464,17 @@ def test_valid_status_deletion_and_editing():
 
     from Code.app import user_timeline
 
+
+    # Initializes a response object to automate testing
+    # Build the arguments that will be passed to the response object
+    url = '/home/timeline/edit_post/prompt/' + str(new_post_id)
+
+    # TBD
+    response = app.test_client().get(url, follow_redirects=True)
+
+    # A successfully loaded page should return a response status code of 200
+    assert response.status_code == 200
+
     # Initializes a response object to automate testing
     # Build the arguments that will be passed to the response object
     url = '/home/timeline/edit_post/' + str(new_post_id)
