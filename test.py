f04bfafc3ff7634964f7f430a80c794f270d30a5
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-04 14:30:52 -0500

    Update test.py

--- test.py
+++ test.py
@@ -66,14 +66,14 @@ def test_valid_user_login_and_logout():
 
     # Imports the instance of the database (db) initialized in the file "app.py"
     from Code.app import db
-    
-    # Comment out
-    db.drop_all()
 
     # Creates a context object to set up the web application's context
     test_request_context = app.test_request_context()
     # Appends the context object
     test_request_context.push()
+
+    # Comment out
+    db.drop_all()
 
     # Builds the database and creates the tables
     db.create_all()
