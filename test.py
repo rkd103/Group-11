76624364cf7bf1a0ef45dd2894b0633a77047a4c
rkd103 <55257087+rkd103@users.noreply.github.com>
2022-11-04 16:25:27 -0500

    Update test.py

--- test.py
+++ test.py
@@ -507,27 +507,35 @@ def test_sending_friend_request_and_verify_obfuscated_foreign_user_content():
     # Imports the database user table "User" and the instance of the bcrypt object initialized in the file "app.py"
     from Code.app import User, bcrypt
 
+    # Creates a temporary user to interface with the web application
+    user_1 = User   (
+                        username = 'js1',
+                        password = bcrypt.generate_password_hash('aA1@sldkepwnwkf'),
+                        first_name = 'James',
+                        middle_name = '',
+                        last_name = 'Smith',
+                        email = 'js1@gmail.com'
+                    )
+    user_2 = User   (
+                    username = 'jm2',
+                    password = bcrypt.generate_password_hash('aA1@sldkepwnwkf'),
+                    first_name = 'Jane',
+                    middle_name = '',
+                    last_name = 'Meredith',
+                    email = 'jm2@gmail.com'
+                    )
 
+    # Saves the newly created user into the database
+    if (1):
+        db.session.add(user_1)
+        db.session.add(user_2)
+        db.session.commit()
 
     # Directly loggs in a user using the specified paramters
     # The user should only be logged in for the test
     # Source: https://github.com/pytest-dev/pytest-flask/issues/40
-    flask_login.login_user(     User   (
-                            username = 'jm2',
-                            password = bcrypt.generate_password_hash('aA1@sldkepwnwkf'),
-                            first_name = 'Jane',
-                            middle_name = '',
-                            last_name = 'Meredith',
-                            email = 'jm2@gmail.com'
-                        ))
-    flask_login.login_user(     User   (
-                                username = 'js1',
-                                password = bcrypt.generate_password_hash('aA1@sldkepwnwkf'),
-                                first_name = 'James',
-                                middle_name = '',
-                                last_name = 'Smith',
-                                email = 'js1@gmail.com'
-                            ))
+    flask_login.login_user(user_1)
+    flask_login.login_user(user_2)
     
     # Initializes a response object to automate testing
     # Build the arguments that will be passed to the response object
